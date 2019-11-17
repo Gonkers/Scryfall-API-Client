a1726765280ea9d0e6f0f2a531b1b6f0097b7803
Michael Wagner <michaelwagner42@outlook.com>
2019-11-17 21:42:50 +0100

    Use new structure

---
+++ src/ScryfallApi.Client/Models/Price.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScryfallApi.Client.Models
+{
+    public class Price : BaseItem
+    {
+        [JsonProperty("usd")]
+        public decimal? Usd { get; set; }
+
+        [JsonProperty("usd_foil")]
+        public decimal? UsdFoil { get; set; }
+
+        [JsonProperty("eur")]
+        public decimal? Eur { get; set; }
+
+        [JsonProperty("eur_foil")]
+        public decimal? EurFoil { get; set; }
+
+        [JsonProperty("tix")]
+        public decimal? Tix { get; set; }
+    }
+}
